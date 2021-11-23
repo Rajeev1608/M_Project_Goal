61ee5d7932d936f271f4d65a8f5080e6d6345e74
Rajeev1608 <94438246+Rajeev1608@users.noreply.github.com>
2021-11-23 16:02:36 +0530

    Update fun.h

--- Project/3_Implementation/inc/fun.h
+++ Project/3_Implementation/inc/fun.h
@@ -1,9 +1,8 @@
 #ifndef _FUN_H
 #define _FUN_H
-void addrecords();
+void addTeam();
 void deleterecords();
-void searchrecords();
+void viewPointTable();
 void listrecords();
-void payment();
-void modifyrecords();
+void Exit();
 #endif
